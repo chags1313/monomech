45abeda3eb962b03dba784b5d2ace7e1c598a88d
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2026-04-08 06:19:36 -0400

    Delete src/monomech directory

--- src/monomech/pose/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mediapipe_backend import MediaPipePoseEstimator
-from .model_zoo import default_pose_model_url, resolve_pose_model_asset
-
-__all__ = ["MediaPipePoseEstimator", "default_pose_model_url", "resolve_pose_model_asset"]
--- src/monomech/recon/__init__.py
+++
@@ -1,4 +0,0 @@
-from .global_pose import estimate_global_pose
-from .pnp import compute_pnp_pose
-
-__all__ = ["compute_pnp_pose", "estimate_global_pose"]
